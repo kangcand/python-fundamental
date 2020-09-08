4090563104c6e67903bf2c031f6afd2d51124e1b
candra <candraherdiansyah14@gmail.com>
2020-09-08 10:16:35 +0700

    Python Fundamentals

---
+++ python_study_2/page4/script.py
@@ -0,0 +1,5 @@
+fruits = ['apel', 'pisang', 'jeruk']
+
+# Dapatkan element fruits menggunakan loop for, dan cetak 'Saya suka ___'
+for fruit in fruits:
+    print('Saya suka '+fruit)
---
+++ python_study_2/page7/script.py
@@ -0,0 +1,5 @@
+fruits = {'apel': 'apple', 'jeruk': 'orange', 'anggur': 'grape'}
+
+# Dengan loop for, cetak '___ adalah ___ dalam bahasa Inggris'
+for fruit in fruits:
+    print(fruit+' adalah '+fruits[fruit]+' dalam bahasa Inggris')
